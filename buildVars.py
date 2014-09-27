e1648273a8cb8702e554c43106448514ef79df97
Noelia Ruiz Martínez <nrm1977@gmail.com>
2014-09-27 21:29:31 +0200

    Updated template according to ticket 2694.

--- buildVars.py
+++ buildVars.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+
 # Build customizations
 # Change this file instead of sconstruct or manifest files, whenever possible.
 
@@ -6,24 +8,27 @@ _ = lambda x : x
 
 # Add-on information variables
 addon_info = {
-	# add-on Name
-	"addon-name" : "eMule",
+	# for previously unpublished addons, please follow the community guidelines at:
+	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt
+	# add-on Name, internal for nvda
+	"addon_name" : "eMule",
+	# Add-on summary, usually the user visible name of the addon.
+	# Translators: Summary for this add-on to be shown on installation and add-on information.
+	"addon_summary" : _("eMule"),
 	# Add-on description
-	# TRANSLATORS: Summary for this add-on to be shown on installation and add-on information.
-	"addon-summary" : _("eMule"),
-	# Add-on description
-	# Translators: Long description to be shown for this add-on on installation and add-on information
-	"addon-description" : _("""Improves eMule's accessibility with NVDA.
-	eMule is a P2P program to search and share files.
-	You can get more information about eMule at
-	http://www.emule-project.net
-	"""),
+	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
+	"addon_description" : _("""Improves eMule's accessibility with NVDA.
+eMule is a P2P program to search and share files.
+You can get more information about eMule at
+http://www.emule-project.net"""),
 	# version
-	"addon-version" : "1.2",
+	"addon_version" : "2.0-dev",
 	# Author(s)
-	"addon-author" : "Noelia <nrm1977@gmail.com>, Chris <llajta2012@hotmail.com>, Alberto <a.buffolino@gmail.com>",
+	"addon_author" : u"Noelia <nrm1977@gmail.com>, Chris <llajta2012@hotmail.com>, Alberto <a.buffolino@gmail.com>",
 	# URL for the add-on documentation support
-	"addon-url" : "http://addons.nvda-project.org"
+	"addon_url" : "http://addons.nvda-project.org",
+	# Documentation file name
+	"addon_docFileName" : "readme.html",
 }
 
 
@@ -34,7 +39,7 @@ import os.path
 pythonSources = [os.path.join("addon", "appModules", "*.py")]
 
 # Files that contain strings for translation. Usually your python sources
-i18nSources = pythonSources + ["buildVars.py", "docHandler.py"]
+i18nSources = pythonSources + ["buildVars.py"]
 
 # Files that will be ignored when building the nvda-addon file
 # Paths are relative to the addon directory, not to the root directory of your addon sources.
